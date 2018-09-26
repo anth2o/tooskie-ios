7589508fb755041521787371098fd5d3b71da7db
Antoine <ahoorelbeke@gmail.com>
2018-09-26 12:07:10 +0200

    Fixed Ingredient

---
+++ tooskie-ios/Ingredient.swift
@@ -0,0 +1,19 @@
+//
+//  Ingredient.swift
+//  tooskie-ios
+//
+//  Created by Antoine Hoorelbeke on 26/09/2018.
+//  Copyright © 2018 Antoine Hoorelbeke. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Ingredient {
+    var name: String
+//    var picture: UIKit.UIImage
+    
+    init() {
+        self.name = "poulet"
+    }
+}
---
+++ tooskie-ios/Pantry.swift
@@ -0,0 +1,13 @@
+//
+//  Pantry.swift
+//  tooskie-ios
+//
+//  Created by Antoine Hoorelbeke on 26/09/2018.
+//  Copyright © 2018 Antoine Hoorelbeke. All rights reserved.
+//
+
+import Foundation
+
+class Pantry{
+
+}
