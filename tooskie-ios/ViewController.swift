7723b8daa2c6798e6cdfb84831786351dceca79c
Antoine <ahoorelbeke@gmail.com>
2018-09-26 06:52:38 +0200

    Initial Commit

---
+++ tooskie-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tooskie-ios
+//
+//  Created by Antoine Hoorelbeke on 26/09/2018.
+//  Copyright © 2018 Antoine Hoorelbeke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
