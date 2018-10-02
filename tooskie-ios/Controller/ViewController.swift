39bf7e48fc17a6a16f5584ad70c455b2c200ed89
Antoine <ahoorelbeke@gmail.com>
2018-10-02 16:46:10 +0200

    Refactor and minor fix

--- tooskie-ios/Controller/ViewController.swift
+++ tooskie-ios/Controller/ViewController.swift
@@ -1,64 +1,26 @@
-//
-//  ViewController.swift
-//  tooskie-ios
-//
-//  Created by Antoine Hoorelbeke on 26/09/2018.
-//  Copyright © 2018 Antoine Hoorelbeke. All rights reserved.
-//
-
 import UIKit
+
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate {
     let tooskiePantry = Pantry(name: "Tooskie pantry")
     var userPantry = Pantry(name: "User pantry")
     var serverConfig = ServerConfig()
-    let loadAllIngredients = true
 
-    
     @IBOutlet weak var pantryTableView: UITableView!
     @IBOutlet weak var ingredientSearchBar: UISearchBar!
-    
+
     @IBAction func launchRecipes(_ sender: Any) {
+        print("Launch")
         self.loadPantry()
     }
     
-    @IBAction func ingredientSearchButton(_ sender: Any) {
-        addIngredient()
-    }
-    
-    func capitalize(string: String) -> String {
-        return string.prefix(1).uppercased() + string.dropFirst()
-    }
-    
-    func addIngredient(){
-        if let text = ingredientSearchBar.text {
-            if self.loadAllIngredients {
-                if let chosenIngredient = self.tooskiePantry.getIngredientByName(ingredientName: text){
-                    self.addAndDisplayNewIngredient(ingredient: chosenIngredient)
-                }
-                else {
-                    self.alertIngredientNotAvailable()
-                }
-            }
-        }
-    }
-    
-    func addAndDisplayNewIngredient(ingredient: Ingredient) {
-        userPantry.addIngredient(ingredient: ingredient)
-        self.reload()
-        self.ingredientSearchBar.text = ""
-    }
-    
-    func alertIngredientNotAvailable() {
-        let alert = UIAlertController(title: "Erreur", message: "Cet ingrédient n'est pas disponible", preferredStyle: UIAlertController.Style.alert)
-        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
-        self.present(alert, animated: true, completion: nil)
+    @IBAction func ingredientSearch(_ sender: Any) {
+        print("Add")
+        self.addIngredient()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        if self.loadAllIngredients {
-            self.loadPantry()
-        }
+        self.loadPantry()
         pantryTableView.delegate = self
         pantryTableView.dataSource = self
         ingredientSearchBar.delegate = self
@@ -96,6 +58,29 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         task.resume()
     }
     
+    func addIngredient(){
+        if let text = ingredientSearchBar.text {
+            if let chosenIngredient = self.tooskiePantry.getIngredientByName(ingredientName: text.capitalize()){
+                self.addAndDisplayNewIngredient(ingredient: chosenIngredient)
+            }
+            else {
+                self.alertIngredientNotAvailable()
+            }
+        }
+    }
+    
+    func addAndDisplayNewIngredient(ingredient: Ingredient) {
+        userPantry.addIngredient(ingredient: ingredient)
+        self.reload()
+        self.ingredientSearchBar.text = ""
+    }
+    
+    func alertIngredientNotAvailable() {
+        let alert = UIAlertController(title: "Erreur", message: "Cet ingrédient n'est pas disponible", preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.userPantry.count
     }
@@ -113,8 +98,8 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     }
     
     func removeIngredient(ingredient: Ingredient) {
-        let potentielIndexIngredient = self.userPantry.getIndex(ingredient: ingredient)
-        if let indexIngredient = potentielIndexIngredient {
+        let potentialIndexIngredient = self.userPantry.getIndex(ingredient: ingredient)
+        if let indexIngredient = potentialIndexIngredient {
             self.userPantry.removeIngredient(ingredient: ingredient)
             let indexPath = IndexPath(item: indexIngredient, section: 0)
             pantryTableView.deleteRows(at: [indexPath], with: .fade)
@@ -122,45 +107,15 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         }
     }
     
-    func requestAndAddIngredient(ingredient: Ingredient) {
-        if ingredient.permaname == nil {
-            print("Ingredient " + ingredient.getName() + " hasn't a valid permaname")
-            return
-        }
-        var urlComponents = URLComponents()
-        urlComponents.scheme = self.serverConfig.getUrlScheme()
-        urlComponents.host = self.serverConfig.getUrlHost()
-        urlComponents.path = "/ingredient/" + ingredient.permaname!
-        guard let url = urlComponents.url else { fatalError("Could not create URL from components") }
-        var request = URLRequest(url: url)
-        request.httpMethod = "GET"
-        let config = URLSessionConfiguration.default
-        let session = URLSession(configuration: config)
-        let task = session.dataTask(with: request) { (responseData, response, responseError) in
-            DispatchQueue.main.async {
-                if let error = responseError {
-                    print(error.localizedDescription)
-                } else if let jsonData = responseData {
-                    let decoder = JSONDecoder()
-                    do {
-                        let ingredient = try decoder.decode(Ingredient.self, from: jsonData)
-                        self.addAndDisplayNewIngredient(ingredient: ingredient)
-                    } catch {
-                        print("Error")
-                    }
-                } else {
-                    let error = NSError(domain: "", code: 0, userInfo: [NSLocalizedDescriptionKey : "Data was not retrieved from request"]) as Error
-                    print(error.localizedDescription)
-                }
-            }
-        }
-        task.resume()
-    }
-    
     private func reload(){
         self.pantryTableView.reloadData()
-        if self.userPantry.count > 0 {
-            try self.pantryTableView.scrollToRow(at: IndexPath(item: self.userPantry.count-1, section: 0), at: .top, animated: true)
+        self.scrollToBottom()
+    }
+    
+    func scrollToBottom(){
+        DispatchQueue.main.async {
+            let indexPath = IndexPath(row: self.userPantry.count-1, section: 0)
+            self.pantryTableView.scrollToRow(at: indexPath, at: .bottom, animated: true)
         }
     }
 }
