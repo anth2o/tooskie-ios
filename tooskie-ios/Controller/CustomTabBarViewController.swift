51e38abdc0a5d0e0a64b7475927065580c50da9f
Antoine <ahoorelbeke@gmail.com>
2019-01-09 13:30:03 +0100

    Selected index on fridge

---
+++ tooskie-ios/Controller/CustomTabBarViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabBarViewController.swift
+//  tooskie-ios
+//
+//  Created by Antoine Hoorelbeke on 09/01/2019.
+//  Copyright © 2019 Antoine Hoorelbeke. All rights reserved.
+//
+
+import UIKit
+
+class CustomTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.selectedIndex = 2
+    }
+}
